b68406e84583adb8f2522b4e83b8d726e0ee5840
Geodecki1p gr1 <113300767+1p22geo@users.noreply.github.com>
2022-09-19 13:17:42 +0200

    Add files via upload

---
+++ Nowy folder/main.js
@@ -0,0 +1,19 @@
+console.log(2 + 2);
+let x = 6
+const y = 10
+function addNumber(number1, number2) {
+    return number1 + number2;
+}
+console.log(addNumber(2, 3));
+/*
+Z czego składa się zmienna
+-nazwa
+-miejsce w pamięci
+-wartość
+-zakres/czas życia
+
+Czy zmiemnna ma typ
+let float user = 25 - w innym języku
+let user = 25 -  javascript
+to nie zmienna ma typ lecz wartość
+*/
