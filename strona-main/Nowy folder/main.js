8b5d355dc0aedb6948085579b9f57ee5fa2ba614
Geodecki1p gr1 <113300767+1p22geo@users.noreply.github.com>
2022-09-21 10:06:23 +0200

    Website design overhaul
    Created new design for website

---
+++ strona-main/Nowy folder/main.js
@@ -0,0 +1,10 @@
+console.log(2 + 2);
+let x = 6
+const y = 10
+function setContentToMain() {
+    document.getElementById("content").src = "something.html";
+}
+function setContentToAbc() {
+    document.getElementById("content").src = "abc.html";
+}
+console.log(addNumber());
